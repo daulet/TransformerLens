0b8d64f003a2bd46808ef37174c2add319c7c8f5
Daulet Zhanguzin <daulet@cohere.com>
2024-05-16 17:31:58 +0000

    works

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -1154,18 +1154,18 @@ def convert_hf_model_config(model_name: str, **kwargs):
             "n_ctx": hf_config.max_position_embeddings,
             "eps": hf_config.layer_norm_eps,
             "d_vocab": hf_config.vocab_size,
+            "gated_mlp": True,
             "act_fn": hf_config.hidden_act,
             "initializer_range": hf_config.initializer_range,
             "normalization_type": "LN", # our impl does it in fp32
             "positional_embedding_type": "rotary",
             "rotary_base": hf_config.rope_theta,
             "rotary_dim": hf_config.hidden_size // hf_config.num_attention_heads,
-            "use_attn_scale": True,
+            "use_attn_scale": True, # TODO confirm
             "n_key_value_heads": hf_config.num_key_value_heads,
-            "gated_mlp": True,
-            "final_rms": True,
-            # torch_dtype
-            # tie_word_embeddings
+            "final_rms": True, # TODO try without
+            "parallel_attn_mlp": True,
+            "use_hook_mlp_in": True, # TODO next try withouyt
             # model_max_length
             # logit_scale
         }
@@ -2698,53 +2698,44 @@ def convert_cohere_weights(phi, cfg: HookedTransformerConfig):
         W_Q = phi.model.layers[l].self_attn.q_proj.weight
         W_K = phi.model.layers[l].self_attn.k_proj.weight
         W_V = phi.model.layers[l].self_attn.v_proj.weight
-        W_Q = einops.rearrange(
-            W_Q, "(n_head d_head) d_model -> n_head d_model d_head", n_head=cfg.n_heads
-        )
-        W_K = einops.rearrange(
-            W_K, "(n_head d_head) d_model  -> n_head d_model d_head", n_head=cfg.n_heads
-        )
-        W_V = einops.rearrange(
-            W_V, "(n_head d_head) d_model  -> n_head d_model d_head", n_head=cfg.n_heads
-        )
+        W_Q = einops.rearrange(W_Q, "(n h) m->n m h", n=cfg.n_heads)
+        W_K = einops.rearrange(W_K, "(n h) m->n m h", n=cfg.n_key_value_heads)
+        W_V = einops.rearrange(W_V, "(n h) m->n m h", n=cfg.n_key_value_heads)
         state_dict[f"blocks.{l}.attn.W_Q"] = W_Q
         state_dict[f"blocks.{l}.attn._W_K"] = W_K
         state_dict[f"blocks.{l}.attn._W_V"] = W_V
 
-        # b_Q = phi.model.layers[l].self_attn.q_proj.bias
-        # b_K = phi.model.layers[l].self_attn.k_proj.bias
-        # b_V = phi.model.layers[l].self_attn.v_proj.bias
-        # b_Q = einops.rearrange(b_Q, "(n_head d_head) -> n_head d_head", n_head=cfg.n_heads)
-        # b_K = einops.rearrange(b_K, "(n_head d_head) -> n_head d_head", n_head=cfg.n_heads)
-        # b_V = einops.rearrange(b_V, "(n_head d_head) -> n_head d_head", n_head=cfg.n_heads)
-        # state_dict[f"blocks.{l}.attn.b_Q"] = b_Q
-        # state_dict[f"blocks.{l}.attn.b_K"] = b_K
-        # state_dict[f"blocks.{l}.attn.b_V"] = b_V
-
-        W_O = phi.model.layers[l].self_attn.o_proj.weight
-        W_O = einops.rearrange(
-            W_O, "d_model (n_head d_head) -> n_head d_head d_model", n_head=cfg.n_heads
+        # No biases
+        state_dict[f"blocks.{l}.attn.b_Q"] = torch.zeros(cfg.n_heads, cfg.d_head, dtype=cfg.dtype)
+        state_dict[f"blocks.{l}.attn._b_K"] = torch.zeros(
+            cfg.n_key_value_heads, cfg.d_head, dtype=cfg.dtype
+        )
+        state_dict[f"blocks.{l}.attn._b_V"] = torch.zeros(
+            cfg.n_key_value_heads, cfg.d_head, dtype=cfg.dtype
         )
 
+        W_O = phi.model.layers[l].self_attn.o_proj.weight
+        W_O = einops.rearrange(W_O, "m (n h)->n h m", n=cfg.n_heads)
         state_dict[f"blocks.{l}.attn.W_O"] = W_O
-        # state_dict[f"blocks.{l}.attn.b_O"] = phi.model.layers[l].self_attn.o_proj.bias
+        # No bias
+        state_dict[f"blocks.{l}.attn.b_O"] = torch.zeros(cfg.d_model, dtype=cfg.dtype)
 
-        # There is no post attention layer norm in Cohere models
-        state_dict[f"blocks.{l}.ln2.w"] = torch.ones_like(state_dict[f"blocks.{l}.ln1.w"])
-        state_dict[f"blocks.{l}.ln2.b"] = torch.zeros_like(state_dict[f"blocks.{l}.ln1.b"])
+        # Layer Norm 1 and 2 are tied.
+        state_dict[f"blocks.{l}.ln2.w"] = state_dict[f"blocks.{l}.ln1.w"]
+        state_dict[f"blocks.{l}.ln2.b"] = state_dict[f"blocks.{l}.ln1.b"]
 
         state_dict[f"blocks.{l}.mlp.W_in"] = phi.model.layers[l].mlp.up_proj.weight.T
         state_dict[f"blocks.{l}.mlp.W_gate"] = phi.model.layers[l].mlp.gate_proj.weight.T
-        # state_dict[f"blocks.{l}.mlp.b_in"] = phi.model.layers[l].mlp.fc1.bias
+        state_dict[f"blocks.{l}.mlp.b_in"] = torch.zeros(cfg.d_mlp, dtype=cfg.dtype)
         state_dict[f"blocks.{l}.mlp.W_out"] = phi.model.layers[l].mlp.down_proj.weight.T
-        # state_dict[f"blocks.{l}.mlp.b_out"] = phi.model.layers[l].mlp.fc2.bias
+        state_dict[f"blocks.{l}.mlp.b_out"] = torch.zeros(cfg.d_model, dtype=cfg.dtype)
 
     state_dict["ln_final.w"] = phi.model.norm.weight
     # state_dict["ln_final.b"] = phi.model.final_layernorm.bias
 
     # TODO tied weights <---------
     state_dict["unembed.W_U"] = phi.lm_head.weight.T
-    # state_dict["unembed.b_U"] = phi.lm_head.bias
+    state_dict["unembed.b_U"] = torch.zeros(cfg.d_vocab, dtype=cfg.dtype)
 
     return state_dict
 
